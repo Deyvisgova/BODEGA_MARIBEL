7bd731770d787eec93e634dddc56df602626d193
Deyvisgova <deyvisgova@gmail.com>
2025-12-03 11:27:50 -0500

    Keep guía selection active for detail modal

--- controlador/controlador_tablas/controlador_tabla_guia_entrada.php
+++ controlador/controlador_tablas/controlador_tabla_guia_entrada.php
@@ -1,13 +1,15 @@
 <?php
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
     @include '../../modelo/config.php';
     require_once __DIR__ . '/../kardex_helper.php';
 
     $id_guia_entrada = (isset($_POST['id_guia_entrada']))?$_POST['id_guia_entrada']:"";
     $fecha_entrada = (isset($_POST['fecha_entrada']))?$_POST['fecha_entrada']:"";
     $descripcion = (isset($_POST['descripcion']))?$_POST['descripcion']:"";
-    $cantidad_entrada = (isset($_POST['cantidad_entrada']))?$_POST['cantidad_entrada']:"";
-    $producto = (isset($_POST['producto']))?$_POST['producto']:"";
-    $provedor = (isset($_POST['provedor']))?$_POST['provedor']:"";
+    $id_proveedor = (isset($_POST['id_proveedor']))?$_POST['id_proveedor']:"";
     $activo = (isset($_POST['activo']))?$_POST['activo']:"";
 
 
@@ -25,21 +27,11 @@
                     header('location: ../../vista/adm/dashboard/tabla_guia_entrada.php');
                 }else{
 
-                    $insert = "INSERT INTO guia_de_entrada(fecha_entrada,descripcion, cantidad_entrada,producto, provedor, activo) VALUES('$fecha_entrada','$descripcion', '$cantidad_entrada', '$producto', '$provedor','$activo')";
+                    $insert = "INSERT INTO guia_de_entrada(fecha_entrada, descripcion, id_proveedor, activo) VALUES('$fecha_entrada','$descripcion', '$id_proveedor', '$activo')";
                     mysqli_query($conn, $insert);
 
-                    $guiaId = mysqli_insert_id($conn);
-                    if ($activo === 'recibido') {
-                        registrarKardex(
-                            $conn,
-                            $fecha_entrada,
-                            $producto,
-                            'entrada',
-                            (int)$cantidad_entrada,
-                            $descripcion,
-                            'GE-' . $guiaId
-                        );
-                    }
+                    $_SESSION['guia_actual_id'] = mysqli_insert_id($conn);
+                    $_SESSION['guia_actual_proveedor'] = $id_proveedor;
 
                     header('location: ../../vista/adm/dashboard/tabla_guia_entrada.php');
                 }
@@ -47,8 +39,11 @@
             case "btnModificar":
                 //falta implementar más...
 
-                $update = "UPDATE guia_de_entrada SET fecha_entrada='$fecha_entrada',descripcion='$descripcion',cantidad_entrada='$cantidad_entrada', producto='$producto', provedor='$provedor',activo='$activo' WHERE id_guia_entrada='$id_guia_entrada'";
+                $update = "UPDATE guia_de_entrada SET fecha_entrada='$fecha_entrada', descripcion='$descripcion', id_proveedor='$id_proveedor', activo='$activo' WHERE id_guia_entrada='$id_guia_entrada'";
                 mysqli_query($conn, $update);
+
+                $_SESSION['guia_actual_id'] = $id_guia_entrada;
+                $_SESSION['guia_actual_proveedor'] = $id_proveedor;
 
                 header('location: ../../vista/adm/dashboard/tabla_guia_entrada.php');
             break;
@@ -56,9 +51,11 @@
                 //Listo.
                 $delete = "DELETE FROM guia_de_entrada WHERE id_guia_entrada = '$id_guia_entrada'";
                 mysqli_query($conn,$delete);
+                unset($_SESSION['guia_actual_id'], $_SESSION['guia_actual_proveedor']);
                 header('location: ../../vista/adm/dashboard/tabla_guia_entrada.php');
             break;
         case "btnCancelar":
+            unset($_SESSION['guia_actual_id'], $_SESSION['guia_actual_proveedor']);
             header('location: ../../vista/adm/dashboard/tabla_guia_entrada.php');
             break;
 
@@ -67,6 +64,8 @@
             $accionAgregar="disabled";
             $accionModificar = $accionEliminar = $accionCancelar = "";
             $mostrarModal= true;
+            $_SESSION['guia_actual_id'] = $id_guia_entrada;
+            $_SESSION['guia_actual_proveedor'] = $id_proveedor;
             break;
     }
 ?>
--- vista/adm/dashboard/pdfs/pdf_guia_entrada.php
+++ vista/adm/dashboard/pdfs/pdf_guia_entrada.php
@@ -4,7 +4,7 @@ require ('../../../../controlador/fpdf/plantilla.php');
 require ('../../../../modelo/config.php');
 
 
-$consulta = "SELECT * FROM guia_de_entrada";
+$consulta = "SELECT g.id_guia_entrada, g.fecha_entrada, g.descripcion, g.activo, p.Nombre_de_la_empresa AS proveedor FROM guia_de_entrada g LEFT JOIN provedor p ON p.id_provedor = g.id_proveedor";
 $resultado = $conn->query($consulta);
 
 $pdf = new PDF("L","mm", array(300,320));   
@@ -22,9 +22,7 @@ $pdf->Ln(10);
 $pdf->Cell(25,5,"Id Guia de Entrada",1,0,"C");
 $pdf->Cell(40,5,"Fecha",1,0,"C");
 $pdf->Cell(70,5,"Descripcion ",1,0,"C");
-$pdf->Cell(30,5,"Cantidad ",1,0,"C");
-$pdf->Cell(30,5,"Producto ",1,0,"C");
-$pdf->Cell(40,5,"Provedor ",1,0,"C");
+$pdf->Cell(70,5,"Proveedor ",1,0,"C");
 $pdf->Cell(20,5,"Estado ",1,1,"C");
 
 
@@ -34,9 +32,7 @@ while ($fila = $resultado->fetch_assoc()){
     $pdf->Cell(25,5,$fila['id_guia_entrada'],1,0,"C");
     $pdf->Cell(40,5,$fila['fecha_entrada'],1,0,"C");
     $pdf->Cell(70,5,strip_tags($fila[utf8_decode('descripcion')]),1,0,"B");
-    $pdf->Cell(30,5,$fila['cantidad_entrada'],1,0,"C");
-    $pdf->Cell(30,5,$fila[utf8_decode('producto')],1,0,"C");
-    $pdf->Cell(40,5,$fila[utf8_decode('provedor')],1,0,"C");
+    $pdf->Cell(70,5,$fila[utf8_decode('proveedor')],1,0,"C");
     $pdf->Cell(20,5,$fila['activo'],1,1,"C");
 
 }
